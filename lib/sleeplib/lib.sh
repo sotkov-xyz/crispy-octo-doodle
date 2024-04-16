0fd4986e2802d4a501538846d34e7939df13b0ab
cuffington <cuffington@sotkov.xyz>
2024-04-16 13:05:52 +0300

    Update sleeplib message

--- lib/sleeplib/lib.sh
+++ lib/sleeplib/lib.sh
@@ -3,7 +3,7 @@
 # Function to sleep for a specified number of seconds and announce the action
 function sleep_and_announce() {
     if [[ -n $1 ]] && [[ $1 =~ ^[0-9]+$ ]]; then
-        echo "Going to sleep for $1 seconds..."
+        echo "Going to sleep for $1 seconds"
         sleep $1
         echo "Done sleeping for now!"
     else
